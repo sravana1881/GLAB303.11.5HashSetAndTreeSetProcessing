b3c396aad5f40511ed3e5c99fe5e2dcf404f0501
sravana karne <sandhyajava42@gmail.com>
2025-01-04 23:45:19 -0600

    Commiting GLAB 303.11.5 HashSet and TreeSet Processing

---
+++ src/TreeSetExamples/TreeSetCitiesCompareToExampleFour.java
@@ -0,0 +1,24 @@
+package TreeSetExamples;
+
+import java.util.TreeSet;
+
+public class TreeSetCitiesCompareToExampleFour {
+    public static void main(String[] args) {
+        // Create a TreeSet with user-defined comparator
+        TreeSet<String> cities = new TreeSet<>(new Cities_Comparator());
+        //add elements to the comparator
+        cities.add("UAE");
+        cities.add("Mumbai");
+        cities.add("NewYork");
+        cities.add("Hyderabad");
+        cities.add("Karachi");
+        cities.add("Xanadu");
+        cities.add("Lahore");
+        cities.add("Zagazig");
+        cities.add("Yingkou");
+
+        //print the contents of TreeSet
+        System.out.println("TreeSet: " + cities);
+    }
+
+}
